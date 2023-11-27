02e08aa767c39c021c4ba007fac97d03b4c14866
tong4627 <yeutong00@gmail.com>
2023-11-27 18:04:50 +0800

    Initial Commit

---
+++ SplitBetterThanWise/SplitBetterThanWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitBetterThanWiseApp.swift
+//  SplitBetterThanWise
+//
+//  Created by YTong on 2023/11/27.
+//
+
+import SwiftUI
+
+@main
+struct SplitBetterThanWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
