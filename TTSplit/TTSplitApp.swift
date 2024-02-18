548a6a7dc6247459d775a5e40ed6d0f8f5ed617d
tong4627 <yeutong00@gmail.com>
2024-02-18 15:38:01 +0800

    Initial Commit

---
+++ TTSplit/TTSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TTSplitApp.swift
+//  TTSplit
+//
+//  Created by YTong on 2024/2/18.
+//
+
+import SwiftUI
+
+@main
+struct TTSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
